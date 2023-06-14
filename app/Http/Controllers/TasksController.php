d3b41f6ebedcc579e8bbd6372f0910e0c471e97c
Ryo0411 <watanabe@nippontect.co.jp>
2023-06-14 11:57:30 +0000

    CURD各画面と機能の実装

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $taskes = Task::all();         // 追加
+
+        // メッセージ一覧ビューでそれを表示
+        return view('taskes.index', [     // 追加
+            'taskes' => $taskes,        // 追加
+        ]);
+    }
+
+    public function create()
+    {
+        $task = new Task;
+
+        // メッセージ作成ビューを表示
+        return view('taskes.create', [
+            'task' => $task,
+        ]);
+    }
+
+    // postでmessages/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        // メッセージを作成
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // getでmessages/idにアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ詳細ビューでそれを表示
+        return view('taskes.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでmessages/id/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('taskes.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // deleteでmessages/idにアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
---
+++ resources/views/taskes/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="prose ml-4">
+        <h2>メッセージ 一覧</h2>
+    </div>
+
+    @if (isset($taskes))
+        <table class="table table-zebra w-full my-4">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($taskes as $task)
+                <tr>
+                    <td><a class="link link-hover text-info" href="{{ route('taskes.show', $task->id) }}">{{ $task->id }}</a></td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {{-- メッセージ作成ページへのリンク --}}
+    <a class="btn btn-primary" href="{{ route('taskes.create') }}">新規メッセージの投稿</a>
+
+@endsection
